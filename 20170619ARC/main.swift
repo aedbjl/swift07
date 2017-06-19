f13a8d2e96d156ee664327c4be5ffcf629ba6e8a
iii-user <iii-user@iii-userde-MacBook-4.local>
2017-06-19 11:16:40 +0800

    Initial Commit

---
+++ 20170619ARC/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  20170619ARC
+//
+//  Created by iii-user on 2017/6/19.
+//  Copyright © 2017年 iii-user. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
